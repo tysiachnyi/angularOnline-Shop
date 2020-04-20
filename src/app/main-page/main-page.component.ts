8ef8321a6cd7cd434b863012c41aad1a98241eb6
Nikita Tysiachnyi <nikita.tysiachnyi@gmail.com>
2020-04-20 14:34:32 +0300

    added product service, and work with template

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from "rxjs";
+import {ProductService} from "../shared/services/product.service";
+import {Product} from "../shared/models/interfaces";
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent implements OnInit, OnDestroy {
+  products: Product = [];
+  sub = Subscription;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private productService: ProductService) {
   }
 
+  ngOnInit(){
+    // @ts-ignore
+    this.productService.getAll().subscribe((response: Product) => {
+      this.products = response;
+      console.log(this.products);
+    })
+  }
+
+
+  ngOnDestroy() {
+    // if (this.pSub) {
+    //   this.pSub.unsubscribe();
+    // }
+
+  }
 }
--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {Product} from "../models/product";
+import {Product} from "../models/interfaces";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor(private http: HttpClient) { }
-
-  getAll(): Observable<Product[]> {
-    return this.http.get('')
+  constructor(private http: HttpClient) {
   }
+
+  getAll(): Observable<Product> {
+    return this.http.get(`http://localhost:4200/assets/data.json`)
+  };
 }
